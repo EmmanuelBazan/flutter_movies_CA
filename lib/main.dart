1eaf23f4d3e0f8a62c96e89fe255cd270472b829
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-09-17 19:40:48 -0600

    Multi provider implementation

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,27 @@ import 'package:http/http.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(
-      Provider<ConnectivityRepository>(
-        create: (context) => ConnectivityRepositoryImpl(
-          Connectivity(),
-          InternetChecker(),
-        ),
-        child: Provider<AuthenticationRepository>(
-          create: (context) => AuthenticationRepositoryImpl(
-            const FlutterSecureStorage(),
-            AuthenticationAPI(
-              Http(
-                apiKey: '7de4526afb410af4f76b69c33bcdc202',
-                baseUrl: 'https://api.themoviedb.org/3',
-                client: Client(),
+      MultiProvider(
+        providers: [
+          Provider<ConnectivityRepository>(
+            create: (_) => ConnectivityRepositoryImpl(
+              Connectivity(),
+              InternetChecker(),
+            ),
+          ),
+          Provider<AuthenticationRepository>(
+            create: (_) => AuthenticationRepositoryImpl(
+              const FlutterSecureStorage(),
+              AuthenticationAPI(
+                Http(
+                  apiKey: '7de4526afb410af4f76b69c33bcdc202',
+                  baseUrl: 'https://api.themoviedb.org/3',
+                  client: Client(),
+                ),
               ),
             ),
           ),
-          child: const MyApp(),
-        ),
+        ],
+        child: const MyApp(),
       ),
     );
